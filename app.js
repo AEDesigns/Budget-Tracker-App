6a48b943074976d4da6cbe1917786bb20d570361
Andrew Elliott <mtgelliott15@gmail.com>
2019-06-05 17:35:12 -0500

    Build Skeleton out for html. Catch IDS in js

---
+++ app.js
@@ -0,0 +1,11 @@
+var rent = document.getElementById('rent');
+var utilities = document.getElementById('utilities');
+var carNote = document.getElementById('carNote');
+var subscriptions = document.getElementById('subscriptions');
+var insurance = document.getElementById('insurance');
+var funStuff = document.getElementById('funStuff');
+var emergency = document.getElementById('emergency');
+var savings = document.getElementById('savings');
+var other = document.getElementById('other');
+
+
