3f7015853246b24b40a659973fbc4374bba45e56
Andrew Elliott <mtgelliott15@gmail.com>
2019-06-06 14:18:45 -0500

    Add event Listener

--- app.js
+++ app.js
@@ -7,5 +7,16 @@ var funStuff = document.getElementById('funStuff');
 var emergency = document.getElementById('emergency');
 var savings = document.getElementById('savings');
 var other = document.getElementById('other');
+let monthlyIncome = document.getElementById('monthly');
+let stateOfRes = document.getElementById('State');
 
+let incomeInput = '';
+let stateResidence = '';
 
+monthlyIncome.addEventListener('keyup', function(e){
+    incomeInput = e.target.value;
+}, false)
+
+stateOfRes.addEventListener('keyup', function(e){
+    stateResidence = e.target.value;
+}, false)
