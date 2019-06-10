1df024e7c8b39a7268ba3fed397e07627c8aabcd
Andrew Elliott <mtgelliott15@gmail.com>
2019-06-10 11:24:44 -0500

    Change event listener to change

--- app.js
+++ app.js
@@ -10,27 +10,32 @@ const OTHER = document.getElementById('other');
 const MONTHLY_INCOME = document.getElementById('monthly');
 const STATE_OF_RES = document.getElementById('State');
 
-let incomeInput = '';
-let stateResidence = '';
-let monthlyRent = '';
-let monthlyUtilities = '';
-let monthlyCarNote = '';
-let monthlySubs = '';
-let monthlyIns = '';
-let funStuff = '';
-let emergencyFund = '';
-let monthlySavings = '';
-let monthlyOther = '';
+let incomeInput;
+let stateResidence;
+let monthlyRent;
+let monthlyUtilities;
+let monthlyCarNote;
+let monthlySubs;
+let monthlyIns;
+let funStuff;
+let emergencyFund;
+let monthlySavings;
+let monthlyOther;
 
-MONTHLY_INCOME.addEventListener('keyup', (e) => incomeInput = e.target.value, false)
-STATE_OF_RES.addEventListener('keyup', (e) => stateResidence = e.target.value, false)
-RENT.addEventListener('keyup', (e) => monthlyRent = e.target.value, false)
-UTILITIES.addEventListener('keyup', (e) => monthlyUtilities = e.target.value, false)
-CAR_NOTE.addEventListener('keyup', (e) => monthlyCarNote = e.target.value, false)
-SUBSCRIPTIONS.addEventListener('keyup', (e) => monthlySubs = e.target.value, false)
-INSURANCE.addEventListener('keyup', (e) => monthlyIns = e.target.value, false)
-FUN_STUFF.addEventListener('keyup', (e) => funStuff = e.target.value, false)
-EMERGENCY.addEventListener('keyup', (e) => emergencyFund = e.target.value, false)
-SAVINGS.addEventListener('keyup', (e) => monthlySavings = e.target.value, false)
-OTHER.addEventListener('keyup', (e) => monthlyOther = e.target.value, false)
+MONTHLY_INCOME.addEventListener('change', (e) => incomeInput = e.target.value, false)
+STATE_OF_RES.addEventListener('change', (e) => stateResidence = e.target.value, false)
+RENT.addEventListener('change', (e) => monthlyRent = e.target.value, false)
+UTILITIES.addEventListener('change', (e) => monthlyUtilities = e.target.value, false)
+CAR_NOTE.addEventListener('change', (e) => monthlyCarNote = e.target.value, false)
+SUBSCRIPTIONS.addEventListener('change', (e) => monthlySubs = e.target.value, false)
+INSURANCE.addEventListener('change', (e) => monthlyIns = e.target.value, false)
+FUN_STUFF.addEventListener('change', (e) => funStuff = e.target.value, false)
+EMERGENCY.addEventListener('change', (e) => emergencyFund = e.target.value, false)
+SAVINGS.addEventListener('change', (e) => monthlySavings = e.target.value, false)
+OTHER.addEventListener('change', (e) => monthlyOther = e.target.value, false)
 
+let arrOfCosts = []
+
+function displayCosts(){
+    
+}
