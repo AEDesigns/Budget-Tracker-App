e5b445ecd0a4a9e4c12c1dcc5f1f509d9a7ee89f
Andrew Elliott <43253284+Critchkn@users.noreply.github.com>
2019-06-12 15:04:58 -0500

    Fix concat bug with parseInt, initialize var to 0

--- app.js
+++ app.js
@@ -11,35 +11,36 @@ const MONTHLY_INCOME = document.getElementById('monthly');
 const STATE_OF_RES = document.getElementById('State');
 
 // Income and State for tax api
-let incomeInput;
+let incomeInput = 0;
 let stateResidence = '';
 
 // Monthly Bills //
-let monthlyRent;
-let monthlyUtilities;
-let monthlyCarNote;
-let monthlySubs;
-let monthlyIns;
-let funStuff;
-let emergencyFund;
-let monthlySavings;
-let monthlyOther;
+let monthlyRent = 0;
+let monthlyUtilities = 0;
+let monthlyCarNote = 0;
+let monthlySubs = 0;
+let monthlyIns = 0
+let funStuff = 0;
+let emergencyFund = 0;
+let monthlySavings = 0;
+let monthlyOther = 0;
 
-MONTHLY_INCOME.addEventListener('change', (e) => incomeInput = e.target.value, false)
+MONTHLY_INCOME.addEventListener('change', (e) => incomeInput = parseInt(e.target.value), false)
 STATE_OF_RES.addEventListener('change', (e) => stateResidence = e.target.value, false)
-RENT.addEventListener('change', (e) => monthlyRent = e.target.value, false)
-UTILITIES.addEventListener('change', (e) => monthlyUtilities = e.target.value, false)
-CAR_NOTE.addEventListener('change', (e) => monthlyCarNote = e.target.value, false)
-SUBSCRIPTIONS.addEventListener('change', (e) => monthlySubs = e.target.value, false)
-INSURANCE.addEventListener('change', (e) => monthlyIns = e.target.value, false)
-FUN_STUFF.addEventListener('change', (e) => funStuff = e.target.value, false)
-EMERGENCY.addEventListener('change', (e) => emergencyFund = e.target.value, false)
-SAVINGS.addEventListener('change', (e) => monthlySavings = e.target.value, false)
-OTHER.addEventListener('change', (e) => monthlyOther = e.target.value, false)
+RENT.addEventListener('change', (e) => monthlyRent = parseInt(e.target.value), false)
+UTILITIES.addEventListener('change', (e) => monthlyUtilities = parseInt(e.target.value), false)
+CAR_NOTE.addEventListener('change', (e) => monthlyCarNote = parseInt(e.target.value), false)
+SUBSCRIPTIONS.addEventListener('change', (e) => monthlySubs = parseInt(e.target.value), false)
+INSURANCE.addEventListener('change', (e) => monthlyIns = parseInt(e.target.value), false)
+FUN_STUFF.addEventListener('change', (e) => funStuff = parseInt(e.target.value), false)
+EMERGENCY.addEventListener('change', (e) => emergencyFund = parseInt(e.target.value), false)
+SAVINGS.addEventListener('change', (e) => monthlySavings = parseInt(e.target.value), false)
+OTHER.addEventListener('change', (e) => monthlyOther = parseInt(e.target.value), false)
 
-let totalMonthCosts;
+let totalMonthCosts = 0;
 
 function displayCosts(){
     totalMonthCosts = monthlyRent + monthlyUtilities + monthlyCarNote + monthlySubs + monthlyIns + funStuff + emergencyFund + monthlySavings + monthlyOther;
     document.getElementById("monthlyBills").innerHTML = totalMonthCosts;
+    console.log(parseInt(totalMonthCosts));
 }
